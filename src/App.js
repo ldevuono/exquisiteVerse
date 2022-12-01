7c1c5d661dc61541ab773ce2f60e786d29208202
lauradevuono <lauradevuono@gmail.com>
2022-12-01 17:51:04 -0500

    playing around with url variables, more testing

--- src/App.js
+++ src/App.js
@@ -1,42 +1,28 @@
-import { useEffect } from 'react';
-// import { useState } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
+import Form from './Form.js'
 import './App.css';
 
 function App() {
 
   //first create our states:
-
   // const [line, setLine] = useState([]);
 
-  //call useEffect with an empty dependency array so it executes its callback on component mount and not again TODO: figure out which dependency array i really need here...
-  useEffect(() => {
-
-    // API call with axios:
-    axios({
-      url: "https://poetrydb.org/random/3/lines,author",
-      method: "GET",
-      dataResponse: "json",
-      params: {
-        format: "json"
-      },
-    })
-      .then((res) => {
-        console.log(res.data)
-        // console.log(res.data[0].lines[1])
-        // setLine(res.data[0].lines[1])
-        // TODO: figure out how to loop through the response to get  a certain number of random lines of poetry
-      });
-  }, []
-  );
+  // function that will get the user's choice from the Form.js component:
+  const chooseNumber = (e, userChoice) => {
+    e.preventDefault();
+    console.log(userChoice)
+  }
 
 
   return (
     <div className="App">
       <header>
-        <h1>exquisite corpse</h1>
+        <h1>exquisite verse</h1>
         <h2>dev branch</h2>
       </header>
+      <Form
+        chooseNumber={chooseNumber}
+      />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,41 @@
-// import { useState } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
-const Form = () => {
 
-    // const [userChoice, setUserChoice] = useState("");
+const Form = (props) => {
+
+    // create state to hold user input when they switch between dropdown options (number of lines of poetry)
+    const [userChoice, setUserChoice] = useState("");
+    // const [line, setLine] = useState([]);
+
 
     const submitHandler = (e) => {
-        console.log("you submitted")
+        props.chooseNumber(e, userChoice)
+        // API call with axios:
+        axios({
+            url: "https://poetrydb.org/random/${userChoice}/lines,author",
+            method: "GET",
+            dataResponse: "json",
+            params: {
+                format: "json"
+            },
+        })
+            .then((res) => {
+                console.log(res.data)
+                // console.log(res.data[0].lines[2])
+
+                // TODO: map through json array based on amount of lines from userInput
+                // filter through array for lines that are not empty strings??
+                //const poem = res.data.filter()
+
+
+                //then setLine(poem);
+                // setLine(res.data[0].lines[1])
+                // TODO: figure out how to loop through the response to get  a certain number of random lines of poetry
+            });
+    }
+    const handleChange = (e) => {
+        setUserChoice(e.target.value)
     }
 
     return (
@@ -18,10 +48,10 @@ const Form = () => {
                 value={userChoice}
             >
                 <option value="" disabled>Pick one:</option>
-                <option value="two">2</option>
-                <option value="three">3</option>
-                <option value="four">4</option>
-                <option value="five">5</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
             </select>
             <button type="submit">Make your poem!</button>
         </form>
