fa1eb5b3f3bd7d6462c464b9e07ea456886c80c8
lauradevuono <lauradevuono@gmail.com>
2022-12-01 09:06:39 -0500

    unused vars for deployment

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
 import './App.css';
 
 function App() {
 
-  //first create our states:
+  // //first create our states:
 
-  const [line, setLine] = useState([]);
+  // const [line, setLine] = useState([]);
 
-  //call useEffect with an empty dependency array so it executes its callback on component mount and not again TODO: figure out which dependency array i really need here...
-  useEffect(() => {
+  // //call useEffect with an empty dependency array so it executes its callback on component mount and not again TODO: figure out which dependency array i really need here...
+  // useEffect(() => {
 
-    // API call with axios:
-    axios({
-      url: "https://poetrydb.org/random/3/lines,author",
-      method: "GET",
-      dataResponse: "json",
-      params: {
-        format: "json"
-      },
-    })
-      .then((res) => {
-        console.log(res.data)
-        // console.log(res.data[0].lines[1])
-        // setLine(res.data[0].lines[1])
-        // TODO: figure out how to loop through the response to get  a certain number of random lines of poetry
-      });
-  }, []
-  );
+  //   // API call with axios:
+  //   axios({
+  //     url: "https://poetrydb.org/random/3/lines,author",
+  //     method: "GET",
+  //     dataResponse: "json",
+  //     params: {
+  //       format: "json"
+  //     },
+  //   })
+  //     .then((res) => {
+  //       console.log(res.data)
+  //       // console.log(res.data[0].lines[1])
+  //       // setLine(res.data[0].lines[1])
+  //       // TODO: figure out how to loop through the response to get  a certain number of random lines of poetry
+  //     });
+  // }, []
+  // );
 
 
   return (
