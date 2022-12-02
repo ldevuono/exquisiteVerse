a6d500ce09fc702cf7d4091a731fa5c55df94f46
lauradevuono <lauradevuono@gmail.com>
2022-12-02 07:35:09 -0500

    testing error handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
+import { useState } from 'react';
+import axios from 'axios';
 import Form from './Form.js'
 import './App.css';
 
 function App() {
 
-  // function that will get the user's choice from the Form.js component:
-  const chooseNumber = (e, userChoice) => {
+  // create state to hold user input when they switch between dropdown options (number of lines of poetry), and to hold the poem the user creates
+  const [userChoice, setUserChoice] = useState("");
+  const [lines, setLines] = useState([]);
+
+  // create a function that will get the user's choice from the dropdown menu and prevent default browser refresh behaviour:
+  const chooseNumber = (e) => {
     e.preventDefault();
-    console.log(userChoice)
+  }
+
+  // function to call the API and parse data on submit
+  const submitHandler = (e) => {
+    chooseNumber(e, userChoice)
+    // API call with axios:
+    axios({
+      url: `https://poetrydb.org/random/${userChoice}/lines,author`,
+      method: "GET",
+      dataResponse: "json",
+      params: {
+        format: "json"
+      },
+    })
+      .then((res) => {
+        const resArray = res.data;
+        resArray.map((line) => {
+          let poem = line.lines[1]
+          console.log(poem)
+          return poem
+        })
+        // putting the created poem into state:
+        setLines(resArray);
+      })
+  };
+
+  // TODO: figure out how to get "pick one" as default on form
+
+
+  // TODO:  filter through array for lines that are not empty strings??
+  // const poem = resArray.filter((line) => {
+  //     return line.lines[1] != ""; ??????
+
+  // creating the function to pass to the onChange eventlistener through props:
+  const handleChange = (e) => {
+    //get user input into state:
+    setUserChoice(e.target.value)
   }
 
 
@@ -18,6 +60,8 @@ function App() {
       </header>
       <Form
         chooseNumber={chooseNumber}
+        handleChange={handleChange}
+        submitHandler={submitHandler}
       />
     </div>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,51 +1,14 @@
-import { useState } from 'react';
-import axios from 'axios';
-
 
 const Form = (props) => {
 
-    // create state to hold user input when they switch between dropdown options (number of lines of poetry)
-    const [userChoice, setUserChoice] = useState("");
-    // const [line, setLine] = useState([]);
-
-
-    const submitHandler = (e) => {
-        props.chooseNumber(e, userChoice)
-        // API call with axios:
-        axios({
-            url: `https://poetrydb.org/random/${userChoice}/lines,author`,
-            method: "GET",
-            dataResponse: "json",
-            params: {
-                format: "json"
-            },
-        })
-            .then((res) => {
-                console.log(res.data)
-                // console.log(res.data[0].lines[2])
-
-                // TODO: map through json array based on amount of lines from userInput
-                // filter through array for lines that are not empty strings??
-                //const poem = res.data.filter()
-
-
-                //then setLine(poem);
-                // setLine(res.data[0].lines[1])
-                // TODO: figure out how to loop through the response to get  a certain number of random lines of poetry
-            });
-    }
-    const handleChange = (e) => {
-        setUserChoice(e.target.value)
-    }
-
     return (
-        <form onSubmit={submitHandler}>
+        <form onSubmit={props.submitHandler}>
             <label htmlFor="lineNumber">How many lines long do you want your poem to be?:</label>
             <select
                 id="lineNumber"
                 name="lineNumber"
-                onChange={handleChange}
-                value={userChoice}
+                onChange={props.handleChange}
+                value={props.userChoice}
             >
                 <option value="" disabled>Pick one:</option>
                 <option value="2">2</option>
