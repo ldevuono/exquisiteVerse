cea9692f482f231059d1cc42e406bfd1deee4ad6
lauradevuono <lauradevuono@gmail.com>
2023-02-11 13:09:10 -0500

    new quill spinner

--- src/components/DisplayPoem.js
+++ src/components/DisplayPoem.js
@@ -1,12 +1,15 @@
 // function to display the poem on the page
-import { BeatLoader } from 'react-spinners';
+// import { BeatLoader } from 'react-spinners';
+import spinner from '../assets/quill.gif';
 
 function DisplayPoem(props) {
     return (
         <section className="poemBox">
             <div className="poem">{props.loading === false ? props.poem.join(" / ")
                 :
-                <BeatLoader className="spinner" color="#cd5c5c" />}</div>
+                // <BeatLoader className="spinner" color="#cd5c5c" />
+                <img className="spinner" src={spinner} alt="quill writing your poem"></img>}
+            </div>
         </section>
     );
 }
